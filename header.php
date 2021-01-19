c4d6fb67c23e72cf2eb0e9ab2cb6321a5ee4d12b
Mantas <mantasabra@gmail.com>
2021-01-19 21:15:26 +0200

    First iteration of the main page

--- header.php
+++ header.php
@@ -1,23 +1,35 @@
 <?php
     echo "
+        <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">
+
         <div class=\"header\">
-            <div class=\"dropdown\">
-                <button class=\"dropbtn\">Activity</button>
-                <div class=\"dropdown-content\">
-                    <a href=\"\">Link 1</a>
+
+
+            <a href=\"index.php\" class=\"home-button\">
+                <i class=\"material-icons\" style=\"font-size: 32px;\">home</i>
+            </a>
+
+            <div class=\"navigation-container\">
+
+                <div class=\"dropdown\">
+                    <button class=\"dropbtn\">Activity</button>
+                    <div class=\"dropdown-content\">
+                        <a href=\"\">Link 1</a>
+                    </div>
                 </div>
-            </div>
 
-            <div class=\"dropdown\">
-                <button class=\"dropbtn\">Skills</button>
-                <div class=\"dropdown-content\">
-                    <a href=\"\">Link 1</a>
+                <div class=\"dropdown\">
+                    <button class=\"dropbtn\">Skills</button>
+                    <div class=\"dropdown-content\">
+                        <a href=\"\">Link 1</a>
+                    </div>
                 </div>
+
+                <button class=\"dropbtn\">Education</button>
+
+                <button class=\"dropbtn\">Contacts</button>
+
             </div>
-
-            <button class=\"dropbtn\">Education</button>
-
-            <button class=\"dropbtn\">Contacts</button>
 
         </div>
     ";
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     include ("header.php");
+    //include ("footer.php");
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +12,23 @@
     </head>
 
     <body>
-        
+        <div class="index-container">
+            <div class="title-text-container">
+                <div class="hairline"></div>
+                <p class="title-text">About me</p> 
+                <div class="hairline"></div>
+            </div>
+            
+            <div class="index-contents-container">
+                <div class="index-contents-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet dignissim tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                </div>
+
+                <div class="index-contents-image">
+                    <img class="index-profile-image" src="/images/me.png" alt="No pic; where did it go doe">
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
