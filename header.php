3a24d810ad90862ffdab302b4f0079a7ce180215
Mantas <mantasabra@gmail.com>
2021-01-19 19:01:17 +0200

    First commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+    echo "
+        <div class=\"header\">
+            <div class=\"dropdown\">
+                <button class=\"dropbtn\">Activity</button>
+                <div class=\"dropdown-content\">
+                    <a href=\"\">Link 1</a>
+                </div>
+            </div>
+
+            <div class=\"dropdown\">
+                <button class=\"dropbtn\">Skills</button>
+                <div class=\"dropdown-content\">
+                    <a href=\"\">Link 1</a>
+                </div>
+            </div>
+
+            <button class=\"dropbtn\">Education</button>
+
+            <button class=\"dropbtn\">Contacts</button>
+
+        </div>
+    ";
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include ("header.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8">
+        <title>Website</title>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
